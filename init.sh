286384f99d1042575cfbabd64cf5b13f262ea8af
syphyr <syphyr@gmail.com>
2023-09-10 21:20:41 +0200

    No backup if mismatched

--- init.sh
+++ init.sh
@@ -6,12 +6,12 @@ git submodule foreach --recursive git clean -dfx
 cd document-viewer/jni/mupdf/mupdf
 make generate
 #patch -p1 < ../overrides/fonts.patch
-patch -p1 < ../overrides/nightmode_slowcmyk.patch
-patch -p1 < ../overrides/cve-2017-14685.patch
-patch -p1 < ../overrides/cve-2017-14686.patch
-patch -p1 < ../overrides/cve-2017-14687.patch
-patch -p1 < ../overrides/cve-2017-15587.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/nightmode_slowcmyk.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/cve-2017-14685.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/cve-2017-14686.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/cve-2017-14687.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/cve-2017-15587.patch
 
 cd ../../djvu/djvulibre
-patch -p1 < ../overrides/remove_print_save_api.patch
-patch -p1 < ../overrides/compatibility.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/remove_print_save_api.patch
+patch -p1 --no-backup-if-mismatch < ../overrides/compatibility.patch
